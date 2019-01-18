bcead244c344e9686d4d1caf28fb40a49eaf30ae
John <29266648+synchronocy@users.noreply.github.com>
2019-01-18 10:34:43 +0930

    Update main.py

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ import sys
 
 
 client = discord.Client()
-YOUR_TOKEN = 'YourTokenhere'
+YOUR_TOKEN = "yourtokenhere"
 
 now = datetime.datetime.now()
 times = datetime.time(now.hour, now.minute, now.second)
@@ -15,56 +15,58 @@ times = datetime.time(now.hour, now.minute, now.second)
 # Post
 @client.event
 async def on_ready():
-    global now
-    global times
-    now = datetime.datetime.now()
-    path = 'logs/'+str(client.user.id)
-    def create():
-        if not os.path.exists(path):
-            os.mkdir(path)
-            with open('logs/'+str(client.user.id)+'/'+client.user.name+'DMS'+str(now.day)+'-'+str(now.month)+'-'+str(now.year)+'.txt','a') as a:
-                with open('logs/'+str(client.user.id)+'/'+client.user.name+'-'+str(now.day)+'-'+str(now.month)+'-'+str(now.year)+'.txt','a') as b:
-                    a.write('User: '+str(client.user.name)+'\n'+'UserID: '+str(client.user.id)+'\n'+'Email: '+str(client.email)+'\n'+'Date: '+str(now.day)+'/'+str(now.month)+'/'+str(now.year)+'\n'+'Member of:\n')
-                    b.write('User: '+str(client.user.name)+'\n'+'UserID: '+str(client.user.id)+'\n'+'Email: '+str(client.email)+'\n'+'Date: '+str(now.day)+'/'+str(now.month)+'/'+str(now.year)+'\n'+'Member of:\n')
-                    print('Created new user!') # new improvements just tidied up the code
-                    for x in client.servers:
-                        a.write('( '+str(x.id)+' ) '+str(x)+'\n')
-                        b.write('( '+str(x.id)+' ) '+str(x)+'\n')
-                    a.write('-----------')
-                    b.write('-----------')
-
-        else:
-            pass
     print('Logged in as')
     print('User: '+str(client.user.name)+'\n'+'UserID: '+str(client.user.id)+'\n'+'Email: '+str(client.email)+'\n'+'Member of:')
     for x in client.servers:
         print('( '+str(x.id)+' ) '+str(x))
     print('\nDate: '+str(now.day)+'-'+str(now.month)+'-'+str(now.year))
     print('------------')
-    create()    
-    
+    now = datetime.datetime.now()
+    date = str(now.day)+'-'+str(now.month)+'-'+str(now.year)
+    times = datetime.time(now.hour, now.minute, now.second)
+    prefix = '['+str(times)+"] ( "+str(message.server)+' ) ('+str(message.channel)+" ) "+str(message.author)
+    path = 'logs/'+client.user.id+'/'+client.user.name
+    print(prefix)
+    userdir = 'logs/'+str(client.user.id)+'/'
+    def create():
+        with open(userdir+'DMS'+date+'.txt','a') as a:
+            with open(userdir+date+'.txt','a') as b:
+                a.write('User: '+str(client.user.name)+'\n'+'UserID: '+str(client.user.id)+'\n'+'Email: '+str(client.email)+'\n'+'Date: '+str(now.day)+'/'+str(now.month)+'/'+str(now.year)+'\n'+'Member of:\n')
+                b.write('User: '+str(client.user.name)+'\n'+'UserID: '+str(client.user.id)+'\n'+'Email: '+str(client.email)+'\n'+'Date: '+str(now.day)+'/'+str(now.month)+'/'+str(now.year)+'\n'+'Member of:\n')
+                print('Done!')
+                for x in client.servers:
+                    a.write('( '+str(x.id)+' ) '+str(x)+'\n')
+                    b.write('( '+str(x.id)+' ) '+str(x)+'\n')
+                    a.write('-----------')
+                    b.write('-----------')
+    create()
+
 @client.event
 async def on_message(message):
     mattach = message.attachments
-    global now
-    global times
     now = datetime.datetime.now()
+    date = str(now.day)+'-'+str(now.month)+'-'+str(now.year)
     times = datetime.time(now.hour, now.minute, now.second)
-    if not mattach: # check if the message is an upload.
-        pass
-    else: # if it is get the url ~ plan to download
+    prefix = '['+str(times)+"] ( "+str(message.server)+' ) ( '+str(message.channel)+" ) "+str(message.author)
+    userdir = 'logs/'+str(client.user.id)+'/'
+    if mattach:
         for x in mattach:
-            #print(x.get('url'))
-            print('File Upload Detected: \n'+str(times)+" ("+str(message.channel)+") "+str(message.author)+' Uploaded: '+x.get('url'))
-            with open('logs/'+str(client.user.id)+'/'+'Uploaded-'+str(now.day)+'-'+str(now.month)+'-'+str(now.year)+'.txt','a') as handle:
-                handle.write('\n'+str(times)+" ("+str(message.server)+') ('+str(message.channel)+") "+str(message.author)+' Uploaded: '+x.get('url'))
-    if 'Direct' or 'None' in str(message.channel):
-        with open('logs/'+str(client.user.id)+'/'+client.user.name+'DMS'+str(now.day)+'-'+str(now.month)+'-'+str(now.year)+'.txt','a') as handle:
-            print(str(times)+" ("+str(message.channel)+") "+str(message.author)+' : '+str(message.content))
-            handle.write('\n'+str(times)+" ("+str(message.channel)+") "+str(message.author)+' : '+str(message.content))            
-    else:
-        with open('logs/'+str(client.user.id)+'/'+client.user.name+'-'+str(now.day)+'-'+str(now.month)+'-'+str(now.year)+'.txt','a') as handle:
-                print("("+str(message.server)+') '+'('+str(message.channel)+") "+str(message.author)+' : '+str(message.content))
-                handle.write('\n'+str(times)+" ("+str(message.server)+') '+'('+str(message.channel)+") "+str(message.author)+' : '+str(message.content))         
-
-client.run(YOUR_TOKEN, bot=False) # remove bot=False if you're logging
+            print('File Upload Detected!\n'+prefix+' Uploaded: '+x.get('url'))
+            with open(userdir+'Uploaded'+date+'.txt','a') as handle:
+                try:
+                    handle.write(prefix+' : '+x.get('url'))
+                except Exception:
+                    return ""
+    '''if 'Direct' or 'None' in str(message.channel):
+        with open(userdir+date+'.txt','a') as handle:
+            try:
+                handle.write(prefix+': '+str(message.content))
+            except Exception:
+                return ""'''# broken need to fix this function
+    with open(userdir+date+'.txt','a') as handle:
+        try:
+            print(prefix+': '+str(message.content))
+            handle.write(prefix+': '+str(message.content)+'\n')
+        except Exception:
+            return ""
+client.run(YOUR_TOKEN, bot=False)
