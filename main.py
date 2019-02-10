fb2c4ec22599fde748a42ffe47e7867de1801ded
John <29266648+synchronocy@users.noreply.github.com>
2019-02-10 19:14:50 +0930

    whoopsies replaced with old build

--- main.py
+++ main.py
@@ -4,73 +4,69 @@ import datetime
 import os
 import uuid
 import sys
-
+import requests
 
 client = discord.Client()
 YOUR_TOKEN = "Yourtokenhere"
 
 now = datetime.datetime.now()
 times = datetime.time(now.hour, now.minute, now.second)
-if not os.path.isdir('./logs'):
-    os.makedirs('./logs')
+
 # Post
 @client.event
 async def on_ready():
-    global now
-    global times
     now = datetime.datetime.now()
-    path = 'logs/'+str(client.user.id)
-    if not os.path.isdir('./logs/'+str(client.user.id)+'/'):
-        os.makedirs('./logs/'+str(client.user.id)+'/')
-    with open('logs/'+str(client.user.id)+'/'+client.user.name+'DMS'+str(now.day)+'-'+str(now.month)+'-'+str(now.year)+'.txt','a') as a:
-        with open('logs/'+str(client.user.id)+'/'+client.user.name+'-'+str(now.day)+'-'+str(now.month)+'-'+str(now.year)+'.txt','a') as b:
-            a.write('User: '+str(client.user.name)+'\n'+'UserID: '+str(client.user.id)+'\n'+'Email: '+str(client.email)+'\n'+'Date: '+str(now.day)+'/'+str(now.month)+'/'+str(now.year)+'\n'+'Member of:\n')
-            b.write('User: '+str(client.user.name)+'\n'+'UserID: '+str(client.user.id)+'\n'+'Email: '+str(client.email)+'\n'+'Date: '+str(now.day)+'/'+str(now.month)+'/'+str(now.year)+'\n'+'Member of:\n')
-            for x in client.servers:
-                try:
-                    a.write('( '+str(x.id)+' ) '+str(x)+'\n')
-                    b.write('( '+str(x.id)+' ) '+str(x)+'\n')
-                except Exception:
-                    print('Error Writing server name to file.')
-            b.write('---------\n')
-        a.write('----------\n')
     print('Logged in as')
     print('User: '+str(client.user.name)+'\n'+'UserID: '+str(client.user.id)+'\n'+'Email: '+str(client.email)+'\n'+'Member of:')
     for x in client.servers:
         print('( '+str(x.id)+' ) '+str(x))
     print('\nDate: '+str(now.day)+'-'+str(now.month)+'-'+str(now.year))
     print('------------')
+    date = str(now.day)+'-'+str(now.month)+'-'+str(now.year)
+    times = datetime.time(now.hour, now.minute, now.second)
+    prefix = '['+str(times)+"] ( "+str(message.server)+' ) ('+str(message.channel)+" ) "+str(message.author)
+    path = 'logs/'+client.user.id+'/'+client.user.name
+    print(prefix)
+    userdir = 'logs/'+str(client.user.id)+'/'
+    if not os.path.isdir(userdir):
+        os.makedirs(userdir)
+    def create():
+        with open(userdir+'DMS'+date+'.txt','a') as a:
+            with open(userdir+date+'.txt','a') as b:
+                a.write('User: '+str(client.user.name)+'\n'+'UserID: '+str(client.user.id)+'\n'+'Email: '+str(client.email)+'\n'+'Date: '+str(now.day)+'/'+str(now.month)+'/'+str(now.year)+'\n'+'Member of:\n')
+                b.write('User: '+str(client.user.name)+'\n'+'UserID: '+str(client.user.id)+'\n'+'Email: '+str(client.email)+'\n'+'Date: '+str(now.day)+'/'+str(now.month)+'/'+str(now.year)+'\n'+'Member of:\n')
+                print('Done!')
+                for x in client.servers:
+                    a.write('( '+str(x.id)+' ) '+str(x)+'\n')
+                    b.write('( '+str(x.id)+' ) '+str(x)+'\n')
+                    a.write('-----------')
+                    b.write('-----------')
+    create()
 
 @client.event
 async def on_message(message):
     mattach = message.attachments
-    global now
-    global times
     now = datetime.datetime.now()
+    date = str(now.day)+'-'+str(now.month)+'-'+str(now.year)
     times = datetime.time(now.hour, now.minute, now.second)
-    if not mattach:
-        pass
-    else:
+    prefix = '['+str(times)+"] ( "+str(message.server)+' ) ( '+str(message.channel)+" ) "+str(message.author)
+    userdir = 'logs/'+str(client.user.id)+'/'
+    if mattach:
         for x in mattach:
-            with open('logs/'+str(client.user.id)+'/'+'Uploaded-'+str(now.day)+'-'+str(now.month)+'-'+str(now.year)+'.txt','a') as handle:
+            print('File Upload Detected!\n'+prefix+' Uploaded: '+x.get('url'))
+            response = requests.get(x.get('url'))
+            if response.status_code == 200:
+                with open("logs/"+client.user.id+'/files/'+str(uuid.uuid4())+'.'+x.get('url')[len(x.get('url'))-3:], 'wb') as f:
+                    f.write(response.content)
+            with open(userdir+'Uploaded'+date+'.txt','a') as handle:
                 try:
-                    print('File Upload Detected: \n'+str(times)+" ("+str(message.channel)+") "+str(message.author)+' Uploaded: '+x.get('url'))
-                    handle.write('\n'+str(times)+" ( "+str(message.server)+' ) ( '+str(message.channel)+" ) "+str(message.author)+' Uploaded: '+x.get('url'))
+                    handle.write(prefix+' : '+x.get('url')+'\n')
                 except Exception:
-                    return "Unable to write to file. Unknown ascii type."
-    if 'Direct' or 'None' in str(message.channel):
-        with open('logs/'+str(client.user.id)+'/'+client.user.name+'DMS'+str(now.day)+'-'+str(now.month)+'-'+str(now.year)+'.txt','a') as handle:
-            try:
-                print(str(times)+' ( '+str(message.server)+' ) ( '+str(message.channel)+" ) "+str(message.author)+' : '+str(message.content))
-                handle.write('\n'+str(times)+" ( "+str(message.server)+' ) ( '+str(message.channel)+" ) "+str(message.author)+' : '+str(message.content))
-            except Exception:
-                return ""
-    else:
-        with open('logs/'+str(client.user.id)+'/'+client.user.name+'-'+str(now.day)+'-'+str(now.month)+'-'+str(now.year)+'.txt','a') as handle:
-            try:
-                print("( "+str(times)+" ( "+str(message.server)+' ) ( '+str(message.channel)+" ) "+str(message.author)+' : '+str(message.content))
-                handle.write('\n'+str(times)+" ( "+str(message.server)+' ) ( '+str(message.channel)+" ) "+str(message.author)+' : '+str(message.content))
-            except Exception:
-                return ""
-
+                    return ""
+    with open(userdir+date+'.txt','a') as handle:
+        try:
+            print(prefix+': '+str(message.content))
+            handle.write(prefix+': '+str(message.content)+'\n')
+        except Exception:
+            return ""
 client.run(YOUR_TOKEN, bot=False)
